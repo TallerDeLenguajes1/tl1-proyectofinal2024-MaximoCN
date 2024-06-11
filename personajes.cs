1b95822d50aa83a0d1b5491d0f6da4ae4229fe87
Maximo <maxicampos178@gmail.com>
2024-06-11 13:50:15 -0300

    Intento en clase 1)

---
+++ personajes.cs
@@ -0,0 +1,34 @@
+
+namespace Personajes;
+public class Personaje
+{
+    public class Caracteristicas ;
+    public class Datos;
+}
+
+
+public class Caracteristicas
+{
+
+    private int salud;
+
+    public int Salud { get => salud; set => salud = value; }
+}
+
+public class Datos
+{
+    private string Apodo;
+    public string Apodo1 { get => Apodo; set => Apodo = value; }
+    
+    
+}
+
+ public class FabricaDePersonajes
+ {
+    public Personaje CrearPerso() 
+    {
+   
+    }
+
+    
+ }
